3b940e458261fde22765d960778682eefcb1e9af
bukadp <thedizain@gmail.com>
2022-07-02 19:40:33 +0300

    Handling Post Requests

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 
@@ -11,6 +12,10 @@ const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening on port ${PORT}`);
 });
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.static('styles'));
 
 app.get('/', (req, res) => {
     const title = 'Home'
