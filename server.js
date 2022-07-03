60e92ae4e21577e60ddbc75a2dbdc13189b2d276
bukadp <thedizain@gmail.com>
2022-07-03 13:22:22 +0300

    MongoDB Integration

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
+
 const app = express();
 const PORT = 3000;
+const db = 'mongodb+srv://buka:470197@cluster0.onqjxcd.mongodb.net/node-blog?retryWrites=true&w=majority'
 
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => console.log('Connected to DB'))
+    .catch((error) => console.log(error));
 
 app.set('view engine', 'ejs');
 const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
@@ -37,40 +45,40 @@ app.get('/contacts', (req, res) => {
 app.get('/posts/:id', (req, res) => {
     const title = 'Post'
     const post = {
-        id: '1', 
+        id: '1',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
         title: 'Post title',
         date: '05.05.2021',
         author: 'Yauhen',
-      };
+    };
     res.render(createPath('post'), { title, post });
 });
 
 app.get('/posts', (req, res) => {
     const title = 'Posts'
-const posts = [
-    { 
-        id: '1', 
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-        title: 'Post title',
-        date: '05.05.2021',
-        author: 'Yauhen',
-    },
-];
+    const posts = [
+        {
+            id: '1',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+            title: 'Post title',
+            date: '05.05.2021',
+            author: 'Yauhen',
+        },
+    ];
 
     res.render(createPath('posts'), { title, posts });
 });
 
 app.post('/add-post', (req, res) => {
-    const {title, author, text} = req.body;
-    const post = {
-        id: new Date(),
-        date: (new Date()).toLocaleDateString(),
-        title,
-        author,
-        text,
-    }
-    res.render(createPath('post'), { title, post })
+    const { title, author, text } = req.body;
+    const post = new Post({ title, author, text });
+    post
+        .save()
+        .then((result) => { res.send(result); })
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' })
+        })
 })
 
 app.get('/add-post', (req, res) => {
